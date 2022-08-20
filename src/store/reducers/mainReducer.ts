918b82b1ad20a31a1c70a6130eabc5ca0b81fd21
Damir Akhmadullin <damir@damir.local>
2022-08-20 16:47:36 +0300

    Added some redux logix

--- src/store/reducers/mainReducer.ts
+++ src/store/reducers/mainReducer.ts
@@ -1,8 +1,15 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { StateType } from './reducer.types'
+import { StateType, StackType } from './reducer.types'
 
 // INITIAL STATE ------------------------------------------------------------>
 const initialState: StateType = {
+  userName: 'John Smith',
+  userStack: [
+    { id: 0, technologyName: 'Ruby', yearsOfExperience: 2 },
+    { id: 1, technologyName: 'PHP', yearsOfExperience: 6 },
+    { id: 2, technologyName: 'Java Script', yearsOfExperience: 4.5 },
+  ],
+  userAvatar: 'https://resize-media.festival-cannes.com/fit-in/2560x1620/media_image/0001/48/c3c10a98dd47ddf0cba7be6850f0815dae7841a8.jpeg',
   input: '',
 }
 
@@ -12,9 +19,14 @@ const mainSlice = createSlice({
   name: 'webpage-slice',
   initialState,
   reducers: {
+
     updateInput: (state, action: PayloadAction<string>) => {
       state.input = action.payload
     },
+
+    addTechnology: (state, action: PayloadAction<StackType>) => {
+      state.userStack.push({ ...action.payload })
+    }
   },
 })
 /* eslint-enable no-param-reassign */
--- src/store/reducers/reducer.types.ts
+++ src/store/reducers/reducer.types.ts
@@ -1,3 +1,13 @@
+export interface StackType {
+  id: number
+  technologyName: string
+  yearsOfExperience: number
+}
+
 export interface StateType {
+  userName: string
+  userStack: Array<StackType>
+  userAvatar: string
   input: string
 }
+
