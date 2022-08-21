68c551fda49c06e6fb9bfb7882b700807c211c74
Damir Akhmadullin <damir@damir.local>
2022-08-21 19:58:33 +0300

    Basic portfolio component

---
+++ src/components/Portfolio/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+const Portfolio: React.FC = () => (
+  <Typography variant='h1'>Portfolio</Typography>
+)
+export default Portfolio
