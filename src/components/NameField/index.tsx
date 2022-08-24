1142dc6d8863f23f0f5c776525fecc3748e8be8c
Damir Akhmadullin <damir@damir.local>
2022-08-24 21:34:14 +0300

    Amended NameField
    - Refactored logic
    - Added InputField

--- src/components/NameField/index.tsx
+++ src/components/NameField/index.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import { useAppSelector, useActions } from 'hooks'
 import { Box, Typography } from '@mui/material'
-import { CorrectIcon, WrongIcon } from 'style/icons'
-
 import { FormikProps, useFormik } from 'formik'
+
+import { InputField } from 'components'
+import { CorrectIcon, WrongIcon } from 'style/icons'
+import { nameSchema } from 'validationSchemas'
 import { Name, NameInput, ErrorMessage } from './styles'
-import nameSchema from './schema'
 
 interface FormValues {
   input: string
@@ -18,48 +19,25 @@ const NameField: React.FC = () => {
   const { changeName } = useActions()
 
   // LOGIC ------------------------------------------------------------------>
-  const {
-    values, errors, touched, handleSubmit, handleChange, handleBlur
-  }
-    : FormikProps<FormValues> =
-    useFormik<FormValues>({
-      initialValues: {
-        input: ''
-      },
-      validationSchema: nameSchema,
-      // eslint-disable-next-line
-      onSubmit: (values, actions) => {
-        changeName(values.input)
-        toggleEditingMode(false)
-        actions.resetForm()
-      }
-    })
-
   // JSX -------------------------------------------------------------------->
   return (
     <article>
 
       {isInEditingMode
-        ? <Box
-          sx={{ position: 'relative' }}
-          component="form"
-          onSubmit={handleSubmit}>
-          <NameInput
-            id='input'
-            autoFocus
-            required
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.input}
-            placeholder={userName}
-          />
-          {errors.input && touched.input
-            ? <WrongIcon /> : <CorrectIcon />}
-          {errors.input && touched.input
-            ? <ErrorMessage>{errors.input}</ErrorMessage>
-            : null
-          }
-        </Box>
+        ? <InputField
+          validationSchema={nameSchema}
+          action={changeName}
+          handleToggleEditing={toggleEditingMode}
+          placeholder={userName}
+          sx={{
+            fontWeight: '500',
+            lineHeight: '48px',
+            height: '48px',
+            fontSize: '40px',
+            width: '360px',
+          }}
+        />
+
         : <Name onClick={() => toggleEditingMode(true)}>{userName}</Name>
       }
 
