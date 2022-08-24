05cbb506f1f8a5bdccdde3eac6ec3ac72beecc25
Damir Akhmadullin <damir@damir.local>
2022-08-24 21:35:28 +0300

    Updated components/index

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,8 +5,10 @@ import Location from './Location'
 import BlockQuote from './BlockQuote'
 import GoogleMap from './GoogleMap'
 import ContentSection from './ContentSection'
+import InputField from './InputField'
 
 export {
+  InputField,
   ContentSection,
   GoogleMap,
   Header,
