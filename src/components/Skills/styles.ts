b3705e963596cfd49170fb52ffa267c220da8097
Damir Akhmadullin <damir@damir.local>
2022-08-25 22:00:19 +0300

    Deleted some files

--- src/components/Skills/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled'
-import { Box } from '@mui/material'
-
-// eslint-disable-next-line
-export const SkillBox = styled(Box)(() => ({
-  display: 'flex',
-  gap: '8px',
-})) as typeof Box
