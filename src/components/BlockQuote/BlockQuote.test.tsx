f8dd6e68160ca4fa281c5c3a9115be873a4080cb
Damir Akhmadullin <damir@damir.local>
2022-09-19 18:19:09 +0300

    Amended BlockQuote tests

--- src/components/BlockQuote/BlockQuote.test.tsx
+++ src/components/BlockQuote/BlockQuote.test.tsx
@@ -8,13 +8,16 @@ import theme from '../../style/theme'
 import { render, screen } from '@testing-library/react'
 import { BlockQuote } from './BlockQuote'
 
+describe('BlockQuote testing', () => {
 
-test('Renders the BlockQuote', () => {
-  render(
-    <ThemeProvider theme={theme}>
-      <BlockQuote>some text for quote</BlockQuote>
-    </ThemeProvider>
-  )
-  const quote = screen.getByText(/some text for quote/i)
-  expect(quote).toBeInTheDocument()
+  test('Renders the BlockQuote correctly', () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <BlockQuote>some text for quote</BlockQuote>
+      </ThemeProvider>
+    )
+    const quote = screen.getByText(/some text for quote/i)
+    expect(quote).toBeInTheDocument()
+  })
 })
+
